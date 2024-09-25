5a7a34f0ccdc230766c18b0a9e47632ebd54eae6
Mirco Spagnolo <Spagnolo_mg@outlook.com>
2024-09-25 12:26:54 +0200

    ajout de la pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,5 +10,8 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(), provideIonicAngular({}), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerImmediately'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           })]
 };
